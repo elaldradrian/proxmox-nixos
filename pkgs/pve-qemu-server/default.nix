10815d26abec476097b55c8ff4eee33f53081f07
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2026-04-03 11:02:02 +0200

    feat: allows virt machine type

--- pkgs/pve-qemu-server/default.nix
+++ pkgs/pve-qemu-server/default.nix
@@ -69,6 +69,10 @@ perl540.pkgs.toPerlModule (
 
     sourceRoot = "${src.name}/src";
 
+    patches = [
+      ./machine-types-non-x86.patch
+    ];
+
     postPatch = ''
       sed -i {qmeventd/,bin/}Makefile \
         -e "/GITVERSION/d" \
